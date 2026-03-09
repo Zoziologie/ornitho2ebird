385937f88893b3974f76d759095cc9f9f3146833
Raphaël Nussbaumer <rafnuss@gmail.com>
2026-03-09 09:05:29 +0100

    Update website

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import packageJson from "./package.json";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/ornitho2ebird/",
   build: {
     rollupOptions: {
       output: {
