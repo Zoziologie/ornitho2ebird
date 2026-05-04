bd537c1135860339b074f6c6984bd8ad2e87b35a
Raphaël Nussbaumer <rafnuss@gmail.com>
2026-05-04 13:02:53 +0200

    fix trailing comma

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -161,6 +161,7 @@ export function speciesComment(speciesCommentTemplate, sightings) {
         })
         .join("");
     })
+    .filter((comment) => String(comment).trim() !== "")
     .join(separator);
 }
 
